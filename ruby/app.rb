63184ab32e7695d0052b9d772683f96c7cd2cad5
4geru <westhouse51@gmail.com>
2021-09-05 11:15:28 +0900

    add ruby example

--- ruby/app.rb
+++ ruby/app.rb
@@ -7,3 +7,52 @@ def client
     }
 end
 
+# 全てのリッチメニューを取得する
+def fetch_rich_menus
+  rich_menu_response = client.get_rich_menus
+  rich_menus = JSON.parse(rich_menu_response.response.body)["richmenus"]
+end
+
+# p fetch_rich_menus
+# => []
+
+# リッチメニューの登録
+# rich_menu_object でリッチメニューの構成を指定する
+# リッチメニューオブジェクト: https://developers.line.biz/ja/reference/messaging-api/#rich-menu-object
+def create_rich_menu(rich_menu_object)
+  client.create_rich_menu(rich_menu_object)
+end
+# リッチメニューの画像登録
+def set_rich_menu_image(rich_menu_id, rich_menu_image_path)
+  client.create_rich_menu_image(rich_menu_id, File.open(rich_menu_image_path))
+end
+
+require './rich_menu_object'
+# # rich menu a を作る
+create_rich_menu_response = create_rich_menu(rich_menu_object_a)
+rich_menu = JSON.parse(create_rich_menu_response.body)
+p rich_menu
+# # => => {"richMenuId"=>"richmenu-xxx"}
+# set_rich_menu_image(rich_menu['richMenuId'], '../public/richmenu-a.png')
+# # rich menu b を作る
+# create_rich_menu_response = create_rich_menu(rich_menu_object_b)
+# rich_menu = JSON.parse(create_rich_menu_response.body)
+# set_rich_menu_image(rich_menu['richMenuId'], '../public/richmenu-b.png')
+# p rich_menu
+# # => => {"richMenuId"=>"richmenu-xxx"}
+
+# 個別のリッチメニューを取得する
+def fetch_rich_menu(rich_menu_id)
+  rich_menu_response = client.get_rich_menu(rich_menu_id)
+  rich_menus = JSON.parse(rich_menu_response.response.body)
+end
+
+# response = fetch_rich_menu(rich_menu['richMenuId'])
+# p response
+
+# 個別のリッチメニューを削除する
+def delete_rich_menu(rich_menu_id)
+  client.delete_rich_menu(rich_menu_id)
+end
+
+# p delete_rich_menu(response['richMenuId'])
---
+++ ruby/rich_menu_object.rb
@@ -0,0 +1,77 @@
+def rich_menu_object_a
+  {
+    "size": {
+      "width": 2500,
+      "height": 1686
+    },
+    "selected": false,
+    "name": "richmenu-a",
+    "chatBarText": "Tap to open",
+    "areas": [
+      {
+        "bounds": {
+          "x": 0,
+          "y": 0,
+          "width": 1250,
+          "height": 1686
+        },
+        "action": {
+          "type": "uri",
+          "uri": "https://developers.line.biz/"
+        }
+      },
+      {
+        "bounds": {
+          "x": 1251,
+          "y": 0,
+          "width": 1250,
+          "height": 1686
+        },
+        "action": {
+          "type": "richmenuswitch",
+          "richMenuAliasId": "richmenu-alias-b",
+          "data": "richmenu-changed-to-b"
+        }
+      }
+    ]
+  }
+end
+
+def rich_menu_object_b
+  {
+    "size": {
+      "width": 2500,
+      "height": 1686
+    },
+    "selected": false,
+    "name": "richmenu-a",
+    "chatBarText": "Tap to open",
+    "areas": [
+      {
+        "bounds": {
+          "x": 0,
+          "y": 0,
+          "width": 1250,
+          "height": 1686
+        },
+        "action": {
+          "type": "richmenuswitch",
+          "richMenuAliasId": "richmenu-alias-a",
+          "data": "richmenu-changed-to-a"
+        }
+      },
+      {
+        "bounds": {
+          "x": 1251,
+          "y": 0,
+          "width": 1250,
+          "height": 1686
+        },
+        "action": {
+          "type": "uri",
+          "uri": "https://developers.line.biz/"
+        }
+      }
+    ]
+  }
+end
